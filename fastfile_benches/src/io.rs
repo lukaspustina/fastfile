dc2292145d92108a24346b16d1b145bfa094a64a
Lukas Pustina <lukas@pustina.de>
2019-08-03 23:23:58 +0200

    Add purge page cache function

--- fastfile_benches/src/io.rs
+++ fastfile_benches/src/io.rs
@@ -1,6 +1,10 @@
 use fastfile::fastfile::MAX_READ_BUF_SIZE;
+use libc;
 use ring::digest::{Context, Digest, SHA256};
 use std::io::{self, Write};
+use std::fs::File;
+use std::path::Path;
+use std::os::unix::io::AsRawFd;
 
 pub struct WriterWithSha<'a, T: Write> {
     inner: &'a mut T,
@@ -76,11 +80,40 @@ impl Write for ConsumingSink {
     }
 }
 
+pub fn purge_cache<T: AsRef<Path>>(path: T) -> io::Result<()> {
+    let file = File::open(path)?;
+    let file_size = file.metadata()?.len();
+    let fd = file.as_raw_fd();
+    unsafe {
+        let mem = libc::mmap(0 as *mut libc::c_void, file_size as libc::size_t, libc::PROT_READ, libc::MAP_SHARED, fd, 0);
+        if mem == libc::MAP_FAILED {
+            eprintln!("mmap failed");
+            return Err(io::Error::from(io::ErrorKind::Other));
+        }
+        let res = libc::msync(mem, file_size as libc::size_t, libc::MS_INVALIDATE);
+        if res < 0 {
+            eprintln!("msync failed");
+            return Err(io::Error::from(io::ErrorKind::Other));
+        }
+    }
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     use spectral::prelude::*;
+
+    #[test]
+    fn test_purge_cache() {
+        let path = "Cargo.toml";
+
+        let res = purge_cache(path);
+
+        asserting("Page cache purged").that(&res).is_ok();
+    }
 
     #[test]
     fn conuming_sink_empty_src() {
