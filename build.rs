fc92156657e0fdc0390c4b1798c58c658edea6ce
Lukas Pustina <lukas@pustina.de>
2019-08-04 18:18:20 +0200

    Moved PAGE_SIZE const into build script

---
+++ build.rs
@@ -0,0 +1,31 @@
+#![feature(rustc_private)]
+extern crate libc;
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufWriter, Write};
+use std::path::Path;
+
+fn main() {
+    let output_dir = env::var("OUT_DIR")
+        .expect("Could not determine OUT_DIR from environment");
+
+    generate_os_consts_file(output_dir)
+        .expect("Failed to write OS consts file");
+}
+
+fn generate_os_consts_file<T: AsRef<Path>>(output_path: T) -> io::Result<()> {
+    let path = output_path.as_ref().join("os_consts.rs");
+    let mut file = BufWriter::new(File::create(&path)?);
+
+    let page_size: usize = get_system_page_size() as usize;
+    writeln!(&mut file, "pub const PAGE_SIZE: usize = {};", page_size)?;
+
+    Ok(())
+}
+
+fn get_system_page_size() -> ::libc::c_long {
+    unsafe {
+        ::libc::sysconf(libc::_SC_PAGESIZE)
+    }
+}
--- src/os/mod.rs
+++ src/os/mod.rs
@@ -8,14 +8,8 @@ pub use macos::read_ahead;
 #[cfg(target_os = "macos")]
 pub use macos::get_page_cache_info;
 
-// TODO: PAGE_SIZE should be either determined during run or even better during compile time
-// 
-// fn get_sys_page_size() -> libc::c_long {
-//     unsafe {
-//         libc::sysconf(libc::_SC_PAGESIZE)
-//     }
-// }
-pub const PAGE_SIZE: usize = 4096;
+// pub const PAGE_SIZE: usize = ???;
+include!(concat!(env!("OUT_DIR"), "/os_consts.rs"));
 
 pub struct PageCacheInfo {
     total: usize,
