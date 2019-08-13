69e9dd157179e8d7cd84f9d5b3fde6f3754cfa5a
Lukas Pustina <lukas@pustina.de>
2019-08-13 18:49:41 +0200

    Next step towards bench analysis

--- fastfile_benches/analysis/install.r
+++ fastfile_benches/analysis/install.r
@@ -1,3 +1,10 @@
 install.packages("optparse", repos='https://cloud.r-project.org/')
 install.packages("rmarkdown", repos='https://cloud.r-project.org/')
-
+install.packages("ggplot2", repos='https://cloud.r-project.org/')
+install.packages("dplyr", repos='https://cloud.r-project.org/')
+install.packages("tidyr", repos='https://cloud.r-project.org/')
+install.packages("forcats", repos='https://cloud.r-project.org/')
+install.packages("hrbrthemes", repos='https://cloud.r-project.org/')
+install.packages("viridis", repos='https://cloud.r-project.org/')
+install.packages("scales", repos='https://cloud.r-project.org/')
+install.packages("gdata", repos='https://cloud.r-project.org/')
